fc09a8081edb1e1c912c6bb8d124af386889130c
mohammedsalah7 <mhamad_m@windowslive.com>
2021-08-17 21:13:49 +0300

    setup for production #16

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const compression = require('compression');
 const router = require('./controllers');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(compression());
+app.disable('x-powered-by');
+app.use(express.static(path.join(__dirname, '..', 'public'), { maxAge: '30d' }));
 
 app.set('port', process.env.PORT || 8080);
 
