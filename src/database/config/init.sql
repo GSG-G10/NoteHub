da8a1d13bdb56a504617b72ab92f2d8944226da4
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-18 13:37:18 +0300

    Modify Schema Relates #30

--- src/database/config/init.sql
+++ src/database/config/init.sql
@@ -1,43 +1,32 @@
 BEGIN;
 
-DROP TABLE IF EXISTS  notes ,category ,category_notes CASCADE;
+DROP TABLE IF EXISTS notes, category CASCADE;
 
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
-   name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE notes (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    content TEXT ,
+    content TEXT,
+    category_id INTEGER NOT NULL REFERENCES category(id),
     date_created DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE category_notes (
-    category_id INTEGER REFERENCES category(id),
-    notes_id INTEGER REFERENCES notes(id)
-);
-
-
 INSERT INTO category (name) VALUES
+('work'),
 ('sport'),
 ('eduction'),
-('personal'),
-('work'),
-('important');
+('important'),
+('personal');
 
-INSERT INTO notes (title,content) VALUES
-('football', 'This is a sport'),
-('school', 'This is a  eduction'),
-('home', 'This is a personal '),
-('developer', 'This is a  work'),
-('important work', 'This is a important ');
-
-INSERT INTO category_notes (notes_id,category_id) VALUES
-(1,2),
-(1,5),
-(2,3),
-(4,5);
+INSERT INTO notes (title, content, category_id) VALUES 
+('sporting', 'This is sport content', 2),
+('eduction', 'This is eduction content', 3),
+('personal', 'This is personal content', 5),
+('work', 'This is work content', 1),
+('important', 'This is important content', 4);
 
 COMMIT;
