9bb12512c067bdb50176242823eeec66bbe8fe8f
mohammedsalah7 <mhamad_m@windowslive.com>
2021-08-18 14:21:12 +0300

    handle add note end point #11

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const displayData = require('./displayData');
+const addNote = require('./addNote');
+const displayNote = require('./displayNote');
 
 const router = express.Router();
 const { errorNotFound, errorServer } = require('./errors');
 
-router.get('/showNotes', displayData);
+router.get('/showNotes', displayNote);
+router.post('/addNote', addNote);
 router.use(errorNotFound);
 router.use(errorServer);
 
