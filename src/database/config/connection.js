ac3748023636d2c4005ab0a071a1e10601190a85
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-18 10:40:12 +0300

    create-db-connection Relates #24

---
+++ src/database/config/connection.js
@@ -0,0 +1,32 @@
+require('env2')('./config.env');
+const { Pool } = require('pg');
+
+const {
+  NODE_ENV, DB_URL, DEV_DB_URL, TEST_DB_URL,
+} = process.env;
+
+let dbURL = '';
+
+switch (NODE_ENV) {
+  case 'production':
+    dbURL = DB_URL;
+    break;
+
+  case 'development':
+    dbURL = DEV_DB_URL;
+    break;
+
+  case 'test':
+    dbURL = TEST_DB_URL;
+    break;
+
+  default:
+    throw new Error('DB Connection Failed!!');
+}
+
+const option = {
+  connectionString: dbURL,
+  ssl: true,
+};
+
+module.exports = new Pool(option);
